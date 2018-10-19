984ebb34d5c9b4bd44fa51dbef7284312c0200ed
ClintonClin <clintonclin00@gmail.com>
2018-10-19 17:34:49 +0300

    commiting my updated modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsComponent } from './forms/forms.component';
-import { Forms1Component } from './forms1/forms1.component';
 import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormsComponent,
-    Forms1Component,
     QuotesComponent
   ],
   imports: [
