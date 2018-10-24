3f0cadbbd934205ce694ca2c0a6fe2e748e4d9af
ClintonClin <clintonclin00@gmail.com>
2018-10-24 09:59:55 +0300

    added modules to modules .ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormsComponent } from './forms/forms.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormsComponent,
-    QuotesComponent
+    QuotesComponent,
+    DetailsComponent,
   ],
   imports: [
     BrowserModule,
